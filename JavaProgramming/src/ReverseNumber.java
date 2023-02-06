688b7ec084c634554dc946dd3fda5c538413eb91
Rabin Basnet <rowbinr@yahoo.com>
2023-02-06 14:05:21 -0700

    reverse a number

--- JavaProgramming/src/ReverseNumber.java
+++ JavaProgramming/src/ReverseNumber.java
@@ -1,4 +1,33 @@
+import java.util.Scanner;
 
 public class ReverseNumber {
 
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a number");
+		
+		int num = sc.nextInt();
+//		int rev = 0;
+//		
+//		while(num!=0) {
+//			rev = rev*10+num%10;
+//			num = num/10;
+//		}
+		
+		// using  StringBuffer class
+//		StringBuffer sb = new StringBuffer(String.valueOf(num));
+//		StringBuffer rev =sb.reverse();
+		
+		// using StringBuilder class
+		StringBuilder sb1 = new StringBuilder();
+		sb1.append(num);
+		StringBuilder rev = sb1.reverse();
+		
+		
+		
+		
+		System.out.println("Reverse number is "+rev);
+		
+	}
 }
